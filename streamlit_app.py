a945f1b3fc2d63bab434781af6b07371a597231e
mattjlunn <mattjlunn@gmail.com>
2022-05-23 16:33:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,9 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
+
+
+
 #New Section to display fruityvice api response
 streamlit.header ('Fruityvice Fruit Advice! ')
 try:
@@ -49,16 +52,24 @@ try:
 except URLError as e:
     streamlit.error()
 
+streamlit.header ("The fruit load list contains:")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit load list")
+    return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load list()
+    streamlit.dataframe (my_data_rows)
+    
+    
+    
 streamlit.stop()
 
 streamlit.write('The user entered', fruit_choice)
-
-
-
-
-
-
-
 
 
 
